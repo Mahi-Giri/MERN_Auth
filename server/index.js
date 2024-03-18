b0ff07c6a3eb28953b2a6edec50f59f159873457
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-19 00:02:01 +0530

    Create a testing route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import express from "express";
 import { DB_NAME } from "../constant.js";
+import userRoute from "./routes/user.route.js";
 
 mongoose
     .connect(`${process.env.MONGO_CONNECTION_STRING}/${DB_NAME}`)
@@ -16,3 +17,5 @@ const app = express();
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
+
+app.use("/api/v1/user", userRoute);

9e6b782922c9c21107b0642fd9791475d94a91ee
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-18 23:44:27 +0530

    Create user model

---
+++ server/models/user.models.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = model("User", userSchema);

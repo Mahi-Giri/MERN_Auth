3183b08a8f8fd6e51daedeaa8e2149e854fa9286
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-18 23:37:21 +0530

    Connect the database to application

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
+import mongoose from "mongoose";
 import express from "express";
+import { DB_NAME } from "../constant.js";
+
+mongoose
+    .connect(`${process.env.MONGO_CONNECTION_STRING}/${DB_NAME}`)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((e) => {
+        console.log(`Error connecting to MongoDB ${e.message}`);
+    });
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });

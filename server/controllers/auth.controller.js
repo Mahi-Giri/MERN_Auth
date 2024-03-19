3599ed3657f7d5529d1e7c908e47214995c160cb
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-19 23:20:22 +0530

    add signin controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import { User } from "../models/User.models.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -18,6 +20,30 @@ export const signup = async (req, res, next) => {
         if (!userAddedInDB) throw new Error(500, "Server down please try again");
 
         return res.status(200).json({ message: "User created successfully" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email) return res.status(404).json({ message: "Please enter a email" });
+    if (!password) return res.status(404).json({ message: "Please enter a password" });
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found"));
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+
+        const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET, {
+            expiresIn: 86400,
+        });
+
+        const { password: hashedPassword, ...rest } = validUser._doc;
+
+        res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
     } catch (error) {
         next(error);
     }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const route = Router();
 
 route.post("/signup", signup);
+route.post("/signin", signin);
 
 export default route;
